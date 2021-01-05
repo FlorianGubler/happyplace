78be16e0b74df7a8899e2aacab3e67e1fbdb3084
Florian Gubler <70200329+FlorianGubler@users.noreply.github.com>
2021-01-05 13:32:58 +0100

    Change to php my admin
    & start with webpage

--- docker-web/sites/index.php
+++ docker-web/sites/index.php
@@ -1,16 +1,38 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="css/style.css">
-    <script src="https://use.fontawesome.com/releases/v5.9.0/js/all.js" data-auto-replace-svg></script>
-    <title>Steckbrief</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.5.0/css/ol.css" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        .map {
+            margin-top: -10px;
+            height: 890px;
+            width: 100%;
+        }
+    </style>
+    <script src="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.5.0/build/ol.js"></script>
+    <title>OpenLayers example</title>
 </head>
-<body id="bodyobj">
-	<div id="Box">
-        <div id="Content">
-                <h2>File: happyplaceprh_db1.mwb</h2>
-        	    <a id="download-file" href="assets/happyplaceprh_db1.mwb" download>Download File</a>
-        </div>
-    </div>
+<body>
+    <form action="/action_page.php">
+        <label for="fname">Name of person to find Location:</label>
+        <input autocomplete="off" type="text" id="fname" name="fname"><br><br>
+    </form>
+    
+    <div id="map" class="map"></div>
+    <script type="text/javascript">
+        var map = new ol.Map({
+            target: 'map',
+            layers: [
+                new ol.layer.Tile({
+                    source: new ol.source.OSM()
+                })
+            ],
+            view: new ol.View({
+                center: ol.proj.fromLonLat([8.52074800985474, 47.36022860676095]),
+                zoom: 18
+            })
+        });
+    </script>
 </body>
 </html>
